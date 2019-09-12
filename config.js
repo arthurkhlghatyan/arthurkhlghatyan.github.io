00266e00cef29a64b96d7affdb39d3d469d30259
Arthur Khlghatyan <arthur.khlghatyan@gmail.com>
2019-09-12 22:07:07 +0400

    Google analytics

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-147707170-1',
   useKatex: false,
   menu: [
     {
